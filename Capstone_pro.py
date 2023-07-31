c4c2795e2216c79bd9ebc71584363b57cfc7f4b1
NOORAIN NAIZ <124370384+NNAIZ@users.noreply.github.com>
2023-07-31 07:53:06 -0400

    Update Capstone_pro.py

--- Capstone_pro.py
+++ Capstone_pro.py
@@ -1,10 +1,8 @@
-# Required libraries
 import streamlit as st
-import pickle
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import OneHotEncoder
 import numpy as np
+import joblib
+from sklearn.ensemble import RandomForestRegressor
 
 # Function to preprocess the car dataset
 def preprocess_car_data(data):
@@ -23,92 +21,63 @@ def preprocess_car_data(data):
 # Function to train the car price prediction model and save it
 def train_and_save_model(X_train, y_train):
     # Train the model (replace this with your actual model training process)
-    model = LinearRegression()
+    model = RandomForestRegressor(random_state=42)
     model.fit(X_train, y_train)
 
-    # Save the trained model using pickle
-    with open('car_price_model.pkl', 'wb') as file:
-        pickle.dump(model, file)
+    # Save the trained model using joblib
+    joblib.dump(model, 'best_model.pkl')
 
 # Function to load the car price prediction model and make predictions
 def predict_car_price(features):
-    # Load the trained model from the pickle file
-    with open('car_price_model.pkl', 'rb') as file:
-        model = pickle.load(file)
+    # Load the trained model from the joblib file
+    loaded_model = joblib.load('best_model.pkl')
 
     # Make predictions using the loaded model
-    price_prediction = model.predict(features)
+    price_prediction = loaded_model.predict(features)
     return price_prediction
-
-# Sample data for demonstration purposes
-# Replace this with your actual dataset for model training
-car_data = {
-    'name': ['Maruti 800 AC', 'Hyundai Verna 1.6 SX'],
-    'year': [2007, 2012],
-    'km_driven': [70000, 100000],
-    'fuel': ['Petrol', 'Diesel'],
-    'seller_type': ['Individual', 'Individual'],
-    'transmission': ['Manual', 'Manual'],
-    'owner': ['First Owner', 'First Owner'],
-    'selling_price': [150000, 500000]
-}
-
-# Create a DataFrame from the sample data
-car_df = pd.DataFrame(car_data)
-
-# Preprocess the car dataset
-X_train, y_train = preprocess_car_data(car_df)
-
-# Train the car price prediction model and save it
-train_and_save_model(X_train, y_train)
 
 # Streamlit app
 def main():
     # Title of the web application
     st.title('Car Price Prediction Model')
 
-    # Display the sample data
-    st.subheader('Sample Data for Model Training')
-    st.write('Features (X_train):')
-    st.write(X_train)
-    st.write('Target (y_train - Car Prices in USD):')
-    st.write(y_train)
+    # Upload the CSV file
+    uploaded_file = st.file_uploader("/content/CAR DETAILS (1v", type=["csv"])
+    if uploaded_file is not None:
+        # Read the uploaded CSV file into a DataFrame
+        car_df = pd.read_csv(uploaded_file)
 
-    # Create a form for user input
-    st.subheader('Enter Car Details')
-    name = st.text_input('Car Name', 'Maruti 800 AC')
-    year = st.number_input('Year of Manufacture', 2000, 2023, 2007)
-    km_driven = st.number_input('Kilometers Driven', 0, 1000000, 70000)
-    fuel = st.selectbox('Fuel Type', ['Petrol', 'Diesel'])
-    seller_type = st.selectbox('Seller Type', ['Individual', 'Dealer', 'Trustmark Dealer'])
-    transmission = st.selectbox('Transmission', ['Manual', 'Automatic'])
-    owner = st.selectbox('Owner', ['First Owner', 'Second Owner', 'Third Owner', 'Fourth & Above Owner'])
+        # Preprocess the car dataset
+        X_train, y_train = preprocess_car_data(car_df)
 
-    # Create a DataFrame with the user input
-    user_data = pd.DataFrame({
-        'name': [name],
-        'year': [year],
-        'km_driven': [km_driven],
-        'fuel': [fuel],
-        'seller_type': [seller_type],
-        'transmission': [transmission],
-        'owner': [owner]
-    })
+        # Train the car price prediction model and save it
+        train_and_save_model(X_train, y_train)
 
-    # Preprocess the user data
-    X_user, _ = preprocess_car_data(user_data)
+        # Display the sample data used for model training
+        st.subheader('Sample Data for Model Training')
+        st.write('Features (X_train):')
+        st.write(X_train)
+        st.write('Target (y_train - Car Prices in USD):')
+        st.write(y_train)
 
-    # Make predictions
-    predicted_price = predict_car_price(X_user)
+        # Select 20 random data points from the original dataset
+        random_subset = car_df.sample(n=20, random_state=42)
 
-    # Display the predictions
-    st.subheader('Car Price Prediction')
-    st.write('Features (X_new):')
-    st.write(X_user)
-    st.write(f'Predicted Car Price: ${predicted_price[0]:,.2f}')
+        # Remove the 'selling_price' column from the random subset
+        subset_for_prediction = random_subset.drop('selling_price', axis=1)
 
-    # Display success message
-    st.success('Car price prediction completed.')
+        # Preprocess the subset for prediction (if needed)
+        subset_for_prediction_encoded = pd.get_dummies(subset_for_prediction, drop_first=True)
+
+        # Make predictions
+        predicted_prices = predict_car_price(subset_for_prediction_encoded)
+
+        # Add the predictions as a new column in the random subset
+        random_subset['predicted_selling_price'] = predicted_prices
+
+        # Display the random subset with predictions
+        st.subheader('Random Subset with Predictions')
+        st.write(random_subset)
 
 if __name__ == '__main__':
     main()
