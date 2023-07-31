22d7703e7200ffc65ac1b1fa0e5b7a7f7f51e324
NOORAIN NAIZ <124370384+NNAIZ@users.noreply.github.com>
2023-07-31 08:40:29 -0400

    Update Capstone_pro.py

--- Capstone_pro.py
+++ Capstone_pro.py
@@ -2,6 +2,16 @@ import streamlit as st
 import pickle
 import numpy as np
 
+# Function to train the model and save it
+def train_and_save_model(X_train, y_train):
+    # Train the model (replace this with your actual model training process)
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    # Save the trained model using pickle
+    with open('best_model.pkl', 'wb') as file:
+        pickle.dump(model, file)
+
 # Function to load the car price prediction model and make predictions
 def predict_car_price(features):
     # Load the trained model from the pickle file
@@ -12,6 +22,14 @@ def predict_car_price(features):
     price_prediction = model.predict(features)
     return price_prediction[0]
 
+# Sample data for demonstration purposes
+# Replace this with your actual dataset for model training
+X_train = np.array([[2007, 70000], [2012, 100000]])
+y_train = np.array([5000, 8000])
+
+# Train the model and save it
+train_and_save_model(X_train, y_train)
+
 # Streamlit app
 def main():
     # Title of the web application
@@ -21,9 +39,6 @@ def main():
     st.subheader('Enter Car Details')
     year = st.number_input('Year of Manufacture', 2000, 2023, 2007)
     km_driven = st.number_input('Kilometers Driven', 0, 1000000, 70000)
-    seller_type = st.selectbox('Seller Type', ['Individual', 'Dealer', 'Trustmark Dealer'])
-    transmission = st.selectbox('Transmission', ['Manual', 'Automatic'])
-    owner = st.selectbox('Owner', ['First Owner', 'Second Owner', 'Third Owner', 'Fourth & Above Owner'])
 
     # Create a numpy array with the user input
     user_data = np.array([[year, km_driven]])
@@ -36,9 +51,6 @@ def main():
     st.write('User Input Data:')
     st.write(f'Year of Manufacture: {year}')
     st.write(f'Kilometers Driven: {km_driven}')
-    st.write(f'Seller Type: {seller_type}')
-    st.write(f'Transmission: {transmission}')
-    st.write(f'Owner: {owner}')
     st.write(f'Predicted Car Price: ${predicted_price:,.2f}')
 
     # Display success message
